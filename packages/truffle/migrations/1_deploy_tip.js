7f29ab533b8e35ce3aeee023fa068aa9e7330b78
clegirar <clemntgirard@gmail.com>
2021-03-21 15:21:51 +0100

    feat: interconnection contracts

--- packages/truffle/migrations/1_deploy_tip.js
+++ packages/truffle/migrations/1_deploy_tip.js
@@ -1,5 +1,5 @@
 const Tip = artifacts.require("Tip");
 
 module.exports = function(deployer) {
-  deployer.deploy(Tip, 10000000000000, 2, 420);
+  deployer.deploy(Tip, 10000000000000, 2, 600, "0x3afB1fd90D9035df0A25313878EE7CA40024233A");
 };
